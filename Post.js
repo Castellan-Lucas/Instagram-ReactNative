7a36d1f4fba3bce05c7905798f2916298e4dff0e
Lucas Castelan <lucascastelanph2@gmail.com>
2023-06-18 12:52:32 -0300

    Projeto Finalizado

---
+++ Post.js
@@ -0,0 +1,108 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons'
+;
+
+const Post = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("./assets/img/rico.jpg")}
+          style={styles.profilePicture}
+        />
+        <Text style={styles.username}>Rigor Rico</Text>
+        <MaterialIcons name="verified" padding={6} size={20} color="blue" />
+      </View>
+      <Image
+        source={require("./assets/img/rigor.jpg")}
+        style={styles.postImage}
+      />
+      <View style={styles.footer}>
+        <View style={styles.iconsContainer}>
+          <Ionicons
+            name="heart-outline"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <Ionicons
+            name="bookmark-outline"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+        </View>
+        <Text style={styles.likes}>500 likes</Text>
+        <Text style={styles.caption}>
+          Descrição do post. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit.Descrição do post. Lorem ipsum dolor sit amet, consectetur adipiscing
+          elit.
+          
+        </Text>
+        <Text style={styles.timestamp}>Há 1 hora</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width:380,
+    backgroundColor: "#fff",
+    top: -200,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+  },
+  profilePicture: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+    marginRight: 8,
+  },
+  username: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  MaterialIcons:{
+    position:'absolute',
+    gap:20
+  },
+  postImage: {
+    width: "100%",
+    height: 300,
+  },
+  footer: {
+    padding: 16,
+  },
+  iconsContainer: {
+    flexDirection: "row",
+    marginBottom: 8,
+  },
+  icon: {
+    marginRight: 16,
+  },
+  likes: {
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  caption: {
+    marginBottom: 8,
+  },
+  timestamp: {
+    color: "#999",
+  },
+});
+
+export default Post;
